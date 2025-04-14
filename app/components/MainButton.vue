763aedeb3bafd1fa7c08a18d7b7fe033d541be26
Palmin <palmin_r@flowwow.com>
2025-04-15 02:01:43 +0300

    KZ-8: Доработка компонента "Кнопка". Раздел Клиника/История

--- app/components/MainButton.vue
+++ app/components/MainButton.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLink
       :to="url"
-      class="inline-block leading-10 px-6 mr-2.5 font-extrabold text-xs text-brand-light uppercase
+      class="inline-block leading-10 px-6 mr-2.5 my-2 font-extrabold text-xs text-brand-light uppercase
       no-underline bg-background-block transition-colors duration-500 hover:bg-background-block-hover"
   >
     <slot>{{ text }}</slot>
