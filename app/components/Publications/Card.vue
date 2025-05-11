7298b9fe36a844a634c8f65d59204568ca7f5c36
Palmin <palmin_r@flowwow.com>
2025-05-11 20:48:40 +0300

    KZ-34 Добавление раздела "Публикации". MVP

---
+++ app/components/Publications/Card.vue
@@ -0,0 +1,58 @@
+<template>
+  <div
+      class="block mb-6 p-6 bg-background-block dark:bg-background-block__dark
+      rounded-lg shadow-md hover:shadow-lg transition-shadow cursor-crosshair">
+
+    <article class="flex gap-6 mb-8">
+      <!-- Изображение слева -->
+      <div class="flex-shrink-0 w-[90x] h-[124px] overflow-hidden rounded-lg">
+        <img
+            :alt="title"
+            :src="titlePhoto"
+            class="object-cover w-full h-full"
+        />
+      </div>
+
+      <!-- Контент справа -->
+      <div class="flex-1">
+        <!-- Дата -->
+        <time class="text-sm ">
+          {{ format.formatDate(date) }}
+        </time>
+
+        <!-- Заголовок -->
+        <h2 class="font-spb text-xl mt-1 mb-2">
+          {{ title }}
+        </h2>
+
+        <!-- Аннотация -->
+        <div
+            v-if="annotation"
+            class="prose mb-3"
+            v-html="format.nl2br(annotation)"
+        />
+
+        <!-- Кнопка -->
+        {{ path }}
+        <MainButton
+            :url="path"
+            text="Прочитать полностью"
+        />
+      </div>
+    </article>
+  </div>
+</template>
+
+<script setup>
+import {useFormatText} from '~/composable/format';
+
+const format = useFormatText();
+
+const props = defineProps({
+  titlePhoto: String,
+  date: String,
+  title: String,
+  annotation: String,
+  path: String
+});
+</script>
---
+++ app/pages/publications/[...slug].vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container mx-auto">
+    <MainButton
+        text="← Все публикации"
+        url="/publications/list"
+    />
+
+    <article class="prose max-w-none mt-2">
+      <header class="mb-6">
+        <div class="flex w-full flex-col md:flex-row md:justify-between mb-4 md:mb-8">
+          <time class="text-sm">
+            {{ format.formatDate(page.date) }}
+          </time>
+          <span class="md:text-right font-bold">{{page.authors}}</span>
+        </div>
+        <h1 class="text-brand-light dark:text-brand-light__dark mb-4 leading-7">
+          {{ page.title }}
+        </h1>
+      </header>
+
+      <!-- Обертка для изображения с обтеканием -->
+      <div v-if="page.titlePhoto" class="float-left mr-4 mb-2">
+        <img
+            :src="page.titlePhoto"
+            alt="Cover"
+            class="rounded-lg shadow-lg max-w-[200px]"
+        >
+      </div>
+
+      <!-- Отображение основного контента -->
+      <ContentRenderer v-if="page" :value="page"/>
+    </article>
+  </div>
+</template>
+
+<script setup>
+import {useFormatText} from '~/composable/format';
+
+const format = useFormatText();
+const route = useRoute();
+
+const {data: page} = await useAsyncData('publication-' + route.path, () => {
+  return queryCollection('publications').path(route.path).first();
+})
+
+if (!page.value) {
+  throw createError({statusCode: 404, statusMessage: 'Page not found', fatal: false});
+}
+</script>
