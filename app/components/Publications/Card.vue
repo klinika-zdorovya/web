63a9e092798ebdfbe579a1375fdfaf0bb052ef63
Palmin <palmin_r@flowwow.com>
2025-05-11 22:04:38 +0300

    KZ-34 Добавление раздела "Публикации"

--- app/components/Publications/Card.vue
+++ app/components/Publications/Card.vue
@@ -1,45 +1,46 @@
 <template>
   <div
-      class="block mb-6 p-6 bg-background-block dark:bg-background-block__dark
-      rounded-lg shadow-md hover:shadow-lg transition-shadow cursor-crosshair">
-
-    <article class="flex gap-6 mb-8">
-      <!-- Изображение слева -->
-      <div class="flex-shrink-0 w-[90x] h-[124px] overflow-hidden rounded-lg">
+      class="flex mb-6 p-6 bg-background-block dark:bg-background-block__dark rounded-lg shadow-md hover:shadow-lg transition-shadow cursor-crosshair">
+    <!-- Текстовый блок -->
+    <div class="overflow-hidden w-full">
+      <!-- Изображение с обтеканием -->
+      <div class="float-left mr-4 mb-0 w-[90px] h-[124px] overflow-hidden rounded-lg">
         <img
             :alt="title"
             :src="titlePhoto"
             class="object-cover w-full h-full"
         />
       </div>
+      <!-- Дата -->
+      <time class="text-sm text-gray-600 dark:text-gray-300 block">
+        {{ format.formatDate(date) }}
+      </time>
 
-      <!-- Контент справа -->
-      <div class="flex-1">
-        <!-- Дата -->
-        <time class="text-sm ">
-          {{ format.formatDate(date) }}
-        </time>
+      <!-- Заголовок -->
+      <h2 class="font-spb text-xl mt-1 mb-2">
+        {{ title }}
+      </h2>
 
-        <!-- Заголовок -->
-        <h2 class="font-spb text-xl mt-1 mb-2">
-          {{ title }}
-        </h2>
+      <!-- Аннотация -->
+      <div
+          v-if="annotation"
+          class="prose dark:prose-invert text-gray-700 dark:text-gray-300 mb-3"
+          v-html="format.nl2br(annotation)"
+      />
 
-        <!-- Аннотация -->
-        <div
-            v-if="annotation"
-            class="prose mb-3"
-            v-html="format.nl2br(annotation)"
-        />
-
-        <!-- Кнопка -->
-        {{ path }}
+      <div class="clear-both flex justify-end ">
         <MainButton
             :url="path"
             text="Прочитать полностью"
+            class="shadow-md"
         />
       </div>
-    </article>
+    </div>
+
+    <!-- Кнопка в правом нижнем углу -->
+    <div class="clear-both relative">
+
+    </div>
   </div>
 </template>
 
@@ -48,7 +49,7 @@ import {useFormatText} from '~/composable/format';
 
 const format = useFormatText();
 
-const props = defineProps({
+defineProps({
   titlePhoto: String,
   date: String,
   title: String,
